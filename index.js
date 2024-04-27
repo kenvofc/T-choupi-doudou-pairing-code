dfd112c7c7fd30760422750d586409758bf1ac61
kenvofc <148454617+kenvofc@users.noreply.github.com>
2024-04-27 21:55:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ res.sendFile(__path + '/main.html')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
-    console.log(`YoutTube: @DGXeon\nTelegram: 𝗞𝗘𝗡~𝗩 𝗖𝗛𝗔𝗧 𝗚𝗥𝗢𝗨𝗣\nGitHub: @kenvofc\nInstsgram: kenv8070\n\nServer running on http://localhost:` + PORT)
+    console.log(`YoutTube: 𝑲𝑬𝑵~𝑽 𝑶𝑭𝑮\nTelegram: 𝗞𝗘𝗡~𝗩 𝗖𝗛𝗔𝗧 𝗚𝗥𝗢𝗨𝗣\nGitHub: @kenvofc\nInstsgram: kenv8070\n\nServer running on http://localhost:` + PORT)
 })
 
 module.exports = app
